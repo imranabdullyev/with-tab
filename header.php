67c4ea1ed4b55eedb3f420dbc50d52ae9e763c72
imran <imran.a@code.edu.az>
2016-04-01 22:12:17 +0400

    tab elave olndu, style ve style1 fayllari birge ist olunub

---
+++ header.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta charset="utf-8i">
+	
+	<!-- Normalise css -->
+	<link rel="stylesheet" href="css/normalise.css">
+	<!-- Bootstrap css -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- FornAwesome css -->
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<!-- Style css -->
+	<link rel="stylesheet" href="css/responsive.css">
+	<link rel="stylesheet" href="css/style.css">
+  	<!-- <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' rel='stylesheet' type='text/css'>
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:' rel='stylesheet' type='text/css'>
+   	<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    	<link rel="stylesheet" type="text/css" href="css/style1.css">
+ 	<script type="text/javascript" src="js/materialize.js"></script> 
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+</head>
+<body>
+
+	<!-- Header Start -->
+	<header id="header">
+		<div class="container">
+
+			<!-- Header Reklam -->
+			<div class="headerReklam">
+				<div class="headerReklami">
+					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=32" width="255"></iframe>
+				</div>
+				<div class="headerReklami">
+					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=33" width="230"></iframe>
+				</div>
+				<div class="headerReklami">
+					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=34" width="230"></iframe>
+				</div>
+				<div class="headerReklami">
+					<iframe border="0" frameborder="0" height="120" marginheight="0" marginwidth="0" scrolling="no" src="http://b.turbo.az/r/?p=207" width="230"></iframe>
+				</div>
+			</div>
+
+			<!-- Header Menu (Dil seçimi və partnor saytlar) -->
+			<div class="headerMenu clearFix">
+				<a href="">bash sehife</a>
+				<ul>
+					<li id="az"><a>azerbaycanca</a></li>
+					<li id="ru"><a>по-русски</a></li>
+					<li id="en"><a>in english</a></li>
+				</ul>
+				<a href="">saytda reklam</a>
+			</div>
+		</div>
+	</header>
+<!--------------header end------------------>
+	
+
+	<!-- Categories -->
+	<section id="categories">
+		<div class="container">
+			<div class="logo">
+			</div>
+			<div class="catNav">
+				<div class="pageVacancies">
+					<a class="active" href="#viewVacancies">İş elanları</a>
+				</div>
+				<div class="pageResumes">
+					<a href="#viewCv">İş axtaranlar</a>
+				</div>
+			</div>
+			<div class="otherNav">
+				<div class="pageAbout">
+					<a href="#resumes">Haqqımızda</a>
+				</div>
+				<div class="add">
+					<button>Elan yerləşdirin</button>
+				</div>
+			</div>
+			<div class="lang-select">
+				<ul>
+					<li id="az"><a>AZ</a></li>
+					<li id="ru"><a>RU</a></li>
+					<li id="en"><a>EN</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<!-- # Categories End -->
+
+
+
+	<!-- # Categories End -->
+	
+
+                <div class="container grey lighten-4">
+                       <div class="col s11">
+                            <div class="row">
+                                  <div class="ara"> </div>
+                                       <div class="col s8">
+                                             <ul class="tabs">
+                                                  <li class="tab col s3" id="haqqim"><a class="active"  href="#test1">Haqqımızda</a></li>
+                                                  <li class="tab col s3 " id="sayt" ><a href="#test2">Saytda Reklam</a>
+                                                  </li>
+                                             </ul>  
+                                       </div>
+                                       <div id="test1" class="col s11">
+                                             <h1>Haqqımızda</h1> 
+                                                  <p><b>Layihə haqqında:</b></p>
+                                                  <p>Boss.Az - əmək bazarının bütün iştirakçılarının faydalana biləcəyi, dəqiq və sürətli iş və ya işçi axtarışı üçün nəzərdə tutulmuş onlayn platformadır. Layihə istifadəçilərə geniş iş elanı və CV bazasından rahat istifadə imkanı yaradır.</p>
+                                                  <p>Layihə fəaliyyətinə 2008-ci ildə başlayıb və hal-hazırda Azərbaycanın əmək bazarında aparıcı onlayn resuslarından biridir.</p>
+                                                  <p>Boss.Az - işədüzəltmə şirkəti deyil. Layihənin məqsədi işə götürən ilə potensial işçi arasında vasitəçisiz və operativ əlaqə imkanı yaratmaqdır.</p>
+                                                  <p><b>Elanların yerləşdirilməsi:</b></p>
+                                                  <p>Boss.Az - istifadəçilərə sayta iş elanı və CV yerləşdirmək imkanı yaradır. Hər istifadəçi ay ərzində ödənişsiz olaraq bir iş elanı və bir CV yerləşdirə bilər.</p>
+                                                  <p><b>Bizimlə əlaqə:</b></p>
+                                                  <p>Biz layihənin inkişafı və təkmilləşdirilməsi üçün əlimizdən gələni edirik və sizin bu haqda olan fikirlərinizi və təkliflərinizi dinləməyə hazırıq. Bizimlə əlaqə yaratmaq üçün <a style="color: #daaf07" href="mailto:boss@boss.az">boss@boss.az</a> e-mail ünvanına yaza və ya <a style="color: #daaf07" href="tel:0124041427">(012) 404-14-27</a> telefon nömrəsinə zəng edə bilərsiniz.</p> 
+                                       </div>        
+                                       <div id="test2" class="col s11">
+                                             <h1>Saytda reklam</h1>
+                                             <p><span style="color:#739e5b">BOSS.AZ - Azərbaycanın ən məşhur və stabil artan internet resurslarından biridir. Hər gün bu saytı minlərlə insan ziyarət edir.</span></p>
+                                             <p><span style="color:#db7443">BOSS.AZ saytında reklam sizin brendləriniz, məhsullarınız, xidmətləriniz, aksiyalarınız və əhəmiyyətli hadisələriniz barədə məlumatı çox geniş auditoriyaya çatdırmaq üçün ən gözəl üsüllarından biridir!</span></p>
+                                             <p><span style="color:#739e5b">Siz həm brendinizin tanınmasının əhəmiyyətli dərəcədə artmasına, həm də məhsullarınız barədə məlumatın geniş yayılmasına nail olacaq, və həmçinin şirkətlərinizin internet saytlarının ziyarət edilməsinin sadə üsülla çoxalması imkanını əldə edəcəksiniz.</span></p>
+                                             <p><span style="color:#739e5b">Sizinlə əməkdaşlığa şad olardıq!</span></p>
+                                             <p><span style="color:#739e5b">GAP </span><span style="color:#db7443">MEDIA <span style="color:#739e5b">— Sizin</span> <span style="color:#db7443">media reklam agentliyiniz</span></span> </p>
+                                             <p><span style="color:#db7443">+99412 594 12 34</span><br> 
+                                             <span style="color:#db7443">+99477 271 44 00</span><br> <a href="mailto:info@gapmedia.az" style="color:#739e5b">info@gapmedia.az</a><br> <a href="http://www.gapmedia.az" style="color:#739e5b">www.gapmedia.az</a>
+                                            </p>                           
+                                       </div>                          
+                                  </div >    
+                            </div>
+                      </div>
+
+
+			
+
+	<!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="footNav">
+						<ul class="footNavbar">
+							<li class="footNavbarItem mobile"><a href="">Mobil Versiyası</a></li>
+							<li class="footNavbarItem"><a href="">Haqqımızda</a></li>
+							<li class="footNavbarItem"><a href="">Saytda reklam</a></li>
+						</ul>
+					</div>
+					<div class="footInfo">
+						<ul class="info">
+							<li class="footCopy"><span>2008-2016 Boss.Az</span></li>
+							<li class="footPhone"><a href="">(012) 000-00-00</a></li>
+							<li class="footEmail"><a href="">boss@boss.az</a></li>
+						</ul>
+					</div>
+					<div class="copyright">
+						<span>
+							Saytın rəhbərliyi reklam bannerlərinin və yerləşdirilmiş elanların məzmununa görə məsuliyyət daşımır.
+						</span>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<a class="title" href="">İş elanları</a>
+					<ul class="footCatList">
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<a class="title" href="">İş axtaranlar</a>
+					<ul class="footCatList">
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+						<li class="cat"><a href="">Category Name</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Footer End -->
+
+	<!-- Jquery -->
+	
+</body>
+</html>
+<script type="text/javascript">
+	// Dil seçimi
+	$(document).ready(function(){
+		$("#az").addClass("lang-selected");
+		$("#az").click(function(){
+			$(".lang-selected").removeClass("lang-selected");
+			$(this).addClass("lang-selected");
+		})
+		$("#ru").click(function(){
+			$(".lang-selected").removeClass("lang-selected");
+			$(this).addClass("lang-selected");
+		})
+		$("#en").click(function(){
+			$(".lang-selected").removeClass("lang-selected");
+			$(this).addClass("lang-selected");
+		})
+	});
+
+	// // Mobil ucun
+
+	// $(document).ready(function(){
+	// 	$("li#az").addClass("lang-selected");
+	// 	$("li#az").click(function(){
+	// 		$(".lang-selected").removeClass("lang-selected");
+	// 		$(this).addClass("lang-selected");
+	// 	})
+	// 	$("li#ru").click(function(){
+	// 		$(".lang-selected").removeClass("lang-selected");
+	// 		$(this).addClass("lang-selected");
+	// 	})
+	// 	$("li#en").click(function(){
+	// 		$(".lang-selected").removeClass("lang-selected");
+	// 		$(this).addClass("lang-selected");
+	// 	})
+	// });
+	// // Tab menu (Vakansiyalar və CV-lər)
+	// var thisAttr; 
+
+	// $(document).ready(function() {
+
+ //    $(".catNav div a").click(function(){
+ //    	$(".active").removeClass("active");
+ //    	$(this).addClass("active");
+ //    	thisAttr = $(this).attr("href");
+ //    	$(".show").removeClass("show");
+ //    	$(thisAttr+"> .container").addClass("show");
+ //    });
+		
+	// });
+
+	// // Yuxarıdan aşağı açılan Axtarış sistemi
+	// var height;
+	// $(document).ready(function() {
+	// 	height = $("#search > .container").height();
+	// 	$("#search").hide();
+
+	// 	$("#searchBtn").on('click', function(event){ 
+ //         	$('#search').slideToggle(100);
+ //    	});
+ //    	$("#searchBtn").click(function(){
+	//     	$("#upDown").toggleClass("fa-caret-up");
+ //    	});
+	// });
+
+
+         $(document).ready(function(){  
+
+    $("#sayt").addClass("sayt"); 
+    $("#sayt").click(function(){
+    $(".sayt").removeClass("sayt");
+    $("#haqqim").addClass("sayt");
+
+ });
+
+    $("#haqqim").click(function(){
+    $("#sayt").addClass("sayt");
+    $("#haqqim").removeClass("sayt");
+
+ });
+
+   $("#sayt").mouseover(function(){
+   $("#sayt").removeClass("sayt");
+
+});
+
+   $("#haqqim").mouseover(function(){
+   $("#haqqim").removeClass("sayt");
+
+});
+
+
+});    
+
+
+</script>
+
+<script type="text/javascript" src="js/materialize.js"> 
+
+            $(document).ready(function(){
+            $('ul.tabs').tabs('select_tab', 'test1');
+ });
+
+
+</script>
